5c1a8bab3c3eae230ca2d297d65067085cfddf21
Yash Garg <22BCS13420@cuchd.in>
2026-05-02 18:57:40 +0530

    Frontend init vite react tailwindcss shadcn

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-medium">Hello Yash</h1>
+      <Button>Test Button</Button>
+    </>
+  )
+}
+
+export default App
