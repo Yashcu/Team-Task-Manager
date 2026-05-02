b5084021c285a5872d55caf6c2481f2e85485178
Yash Garg <22BCS13420@cuchd.in>
2026-05-02 18:57:17 +0530

    backend init basic server typescript

---
+++ backend/src/app.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Health route
+app.get('/hello', (_, res) => {
+    res.json({ message: 'hello from backend' });
+});
+
+export default app;
